7de1e841ed376760284134a11b32a6556e5d3c23
zahid Hassan <zahid13@cse.pstu.ac.bd>
2021-01-18 12:45:44 +0600

    a

---
+++ a/a/a.ctrl.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+    angular
+        .module('app.a')
+        .controller('aCtrl', aCtrl);
+
+    /** @ngInject */
+    function aCtrl() {
+        debugger
+        var TECHDISER_COMPONENT_NAME = "aCtrl";
+        var TECHDISER_SERVICE_INFO = {};
+        var vm = this;
+        vm.loadViewContent = true
+    }
+})();
--- makeMenuDirectories.sh
+++ makeMenuDirectories.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
-# echo Module Name?
-# read moduleName
-# echo Menu Names?
-# read menuNames
-moduleName="template"
-menuNames="menu-1 menu-2 menu-3"
+echo Module Name?
+read moduleName
+echo Menu Names?
+read menuNames
+
+mkdir $moduleName
+cd $moduleName
+
+for menuName in $menuNames; do
+    mkdir $menuName
+    touch $menuName/$menuName.html
+    jsFileName=${menuName//-/.}
+    touch $menuName/$jsFileName.ctrl.js
+
+    name_upper=$(sed -r 's/(^|-)(\w)/\U\2/g' <<<"$menuName")
+    ctrlName="$(tr '[:upper:]' '[:lower:]' <<<${name_upper:0:1})${name_upper:1}"
+    ctrlName+="Ctrl"
+
+    ctrl="(function () {
+    'use strict';
+    angular
+        .module('app.$moduleName')
+        .controller('$ctrlName', $ctrlName);
+
+    /** @ngInject */
+    function $ctrlName() {
+        debugger
+        var TECHDISER_COMPONENT_NAME = "\""$ctrlName"\"";
+        var TECHDISER_SERVICE_INFO = {};
+        var vm = this;
+        vm.loadViewContent = true
+    }
+})();"
+
+    echo "$ctrl" >$menuName/$jsFileName.ctrl.js
+    echo $menuName > $menuName/$menuName.html
+done
 
 module="(function () {
     'use strict';
@@ -20,32 +51,42 @@ module="(function () {
                 abstract: true,
                 url: '/$moduleName'
             })"
-moduleSaveItem="
-        msNavigationServiceProvider.saveItem('$moduleName', {
-            title: '$moduleName',
-            icon: 'icon-desktop-mac',
-            weight: -80
-        });"
 
 for menuName in $menuNames; do
+
+    name_upper=$(sed -r 's/(^|-)(\w)/\U\2/g' <<<"$menuName")
+    foo="$(tr '[:upper:]' '[:lower:]' <<<${name_upper:0:1})${name_upper:1}"
+    foo+="Ctrl"
     menuState="
             .state('app.$moduleName.$menuName', {
-                url: '$menuName',
+                url: '/$menuName',
                 views: {
                     'content@app': {
-                        templateUrl: 'app/main/test.html',
-                        controller: 'ctrl as vm'
+                        templateUrl: 'app/main/$moduleName/$menuName/$menuName.html',
+                        controller: '$foo as vm'
                     }
                 }
             })"
     module+=$menuState
 
 done
+
+moduleName_upper=$(sed 's/[^-]\+/\L\u&/g' <<<"$moduleName")
+moduleTitle=${moduleName_upper//-/ }
+moduleSaveItem="
+        msNavigationServiceProvider.saveItem('$moduleName', {
+            title: '$moduleTitle',
+            icon: 'icon-desktop-mac',
+            weight: -80
+        });"
+
 module+=$moduleSaveItem
 for menuName in $menuNames; do
+    title_upper=$(sed 's/[^-]\+/\L\u&/g' <<<"$menuName")
+    title=${title_upper//-/ }
     menuItem="
         msNavigationServiceProvider.saveItem('$moduleName.$menuName', {
-            title: '$menuName',
+            title: '$title',
             state: 'app.$moduleName.$menuName',
             icon: 'icon-cog-box',
             weight: 1
@@ -61,24 +102,5 @@ last="
 })();"
 
 module+=$last
-
-echo "$module" >module.js
-
-# match="MENUSTATE"
-
-# echo "$module" | sed "s/$match/&$menuState/g"
-# text="loremipsumNEEDLEdolorsitamet"
-# word="HAYSTALK"
-
-# echo "$module" | sed "s/$match/$word/g"
-
-# echo Menu Names?
-# read menuNames
-
-# for menuName in $menuNames; do
-#     mkdir $menuName
-#     touch $menuName/$menuName.html
-#     jsFileName=${menuName//-/.}
-#     touch $menuName/$jsFileName.ctrl.js
-#     echo $module >>$menuName/$jsFileName.ctrl.js
-# done
+moduleFileName=${moduleName//-/.}
+echo "$module" >$moduleFileName.module.js
